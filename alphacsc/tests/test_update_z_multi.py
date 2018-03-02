c916ff8aa489f84acd8a4832be7912e38ce3ee9f
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2018-03-02 18:32:05 +0100

    CLN more (u, v) to uv

--- alphacsc/tests/test_update_z_multi.py
+++ alphacsc/tests/test_update_z_multi.py
@@ -12,11 +12,10 @@ def test_gradient_correctness():
     reg = 0
 
     X = np.random.randn(n_trials, n_channels, n_times)
-    u = np.random.randn(n_atoms, n_channels)
-    v = np.random.randn(n_atoms, n_times_atom)
+    uv = np.random.randn(n_atoms, n_channels + n_times_atom)
     z = np.random.randn(n_atoms, n_trials, n_times_valid)
 
-    update_z_multi(X, u, v, reg, z0=z, solver='l_bfgs', debug=True)
+    update_z_multi(X, uv, reg, z0=z, solver='l_bfgs', debug=True)
 
 
 def test_update_z_multi_decrease_cost_function():
@@ -26,15 +25,14 @@ def test_update_z_multi_decrease_cost_function():
     reg = 0
 
     X = np.random.randn(n_trials, n_channels, n_times)
-    u = np.random.randn(n_atoms, n_channels)
-    v = np.random.randn(n_atoms, n_times_atom)
+    uv = np.random.randn(n_atoms, n_channels + n_times_atom)
     z = np.random.randn(n_atoms, n_trials, n_times_valid)
 
-    loss_0 = compute_X_and_objective_multi(X, z, u, v, reg,
+    loss_0 = compute_X_and_objective_multi(X, z, uv, reg,
                                            feasible_evaluation=False)
 
-    z_hat = update_z_multi(X, u, v, reg, z0=z, solver='l_bfgs')
+    z_hat = update_z_multi(X, uv, reg, z0=z, solver='l_bfgs')
 
-    loss_1 = compute_X_and_objective_multi(X, z_hat, u, v, reg,
+    loss_1 = compute_X_and_objective_multi(X, z_hat, uv, reg,
                                            feasible_evaluation=False)
     assert loss_1 < loss_0
