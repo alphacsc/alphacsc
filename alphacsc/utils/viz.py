91e79b145e469115bf8b1bb8e5111b9f2390937b
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2018-03-23 17:48:40 +0100

    ENH make plot_activations_density scale with activation amplitudes

--- alphacsc/utils/viz.py
+++ alphacsc/utils/viz.py
@@ -10,10 +10,6 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-from sklearn.neighbors import KernelDensity
-from sklearn.model_selection import GridSearchCV
-
-
 from .tools import get_calling_script, positive_hash
 
 colors = ["#4C72B0", "#55A868", "#C44E52", "#8172B2", "#CCB974", "#64B5CD"]
@@ -24,35 +20,19 @@ DEFAULT_CB = {
 }
 
 
-def kde_sklearn(x, x_grid, bandwidth, **kwargs):
+def kde_sklearn(x, x_grid, bandwidth):
     """Kernel Density Estimation with Scikit-learn"""
-    bandwidth = np.atleast_1d(bandwidth)
-
     n_samples, = x.shape
     if n_samples == 0:
         return np.zeros_like(x_grid)
 
-    # Grid-search over bandwidth parameter
-    if bandwidth.size > 1:
-        param_grid = dict(bandwidth=bandwidth)
-        gs = GridSearchCV(KernelDensity(**kwargs), param_grid=param_grid, cv=5)
-        gs.fit(x[:, np.newaxis])
-
-        print('selected bandwidth: %s' % (gs.best_params_, ))
-        kde_skl = gs.best_estimator_
-        bandwidth = kde_skl.bandwidth
-    else:
-        kde_skl = KernelDensity(bandwidth=bandwidth[0], **kwargs)
-        kde_skl.fit(x[:, np.newaxis])
-
-    # score_samples() returns the log-likelihood of the samples
-    log_pdf = kde_skl.score_samples(x_grid[:, np.newaxis])
-    return np.exp(log_pdf) * bandwidth
+    window = np.blackman(bandwidth) * bandwidth
+    return np.convolve(x, window, 'same')
 
 
 def plot_activations_density(Z_hat, n_times_atom, sfreq=1., threshold=0.01,
                              bandwidth='auto', axes=None,
-                             plot_activations=True):
+                             plot_activations=False):
     """
     Parameters
     ----------
@@ -64,7 +44,7 @@ def plot_activations_density(Z_hat, n_times_atom, sfreq=1., threshold=0.01,
         Sampling frequency
     threshold : float
         Remove activations (normalized with the max) below this threshold
-    bandwidth : float, array of float, or in {'auto', 'grid'}
+    bandwidth : float, array of float, or 'auto'
         Bandwidth (in sec) of the kernel
     axes : array of axes, or None
         Axes to plot into
@@ -76,14 +56,8 @@ def plot_activations_density(Z_hat, n_times_atom, sfreq=1., threshold=0.01,
     # sum activations over all trials
     Z_hat_sum = Z_hat.sum(axis=1)
 
-    # normalize to maximum activation
-    if Z_hat_sum.max() != 0:
-        Z_hat_sum /= Z_hat_sum.max()
-
     if bandwidth == 'auto':
-        bandwidth = n_times_atom / float(sfreq) / 4.
-    elif bandwidth == 'grid':
-        bandwidth = n_times_atom / float(sfreq) / 4. * np.logspace(-1, 1, 20)
+        bandwidth = n_times_atom
 
     if axes is None:
         fig, axes = plt.subplots(n_atoms, num='density',
@@ -93,7 +67,7 @@ def plot_activations_density(Z_hat, n_times_atom, sfreq=1., threshold=0.01,
     for ax, activations, color in zip(axes.ravel(), Z_hat_sum, color_cycle):
         ax.clear()
         time_instants = np.arange(n_times_valid) / float(sfreq)
-        selection = activations > threshold
+        selection = activations > threshold * Z_hat_sum.max()
         n_elements = selection.sum()
 
         if n_elements == 0:
@@ -106,13 +80,22 @@ def plot_activations_density(Z_hat, n_times_atom, sfreq=1., threshold=0.01,
                     activations[selection] / activations[selection].max(), '.',
                     color='k')
 
-        # compute the kernel density and plot it
-        kde_x = kde_sklearn(time_instants[selection], time_instants,
-                            bandwidth=bandwidth)
-        ax.fill_between(time_instants, kde_x * n_elements, color=color,
+        window = np.blackman(bandwidth)
+        smooth_activations = np.convolve(activations, window, 'same')
+        ax.fill_between(time_instants, smooth_activations, color=color,
                         alpha=0.5)
 
     return axes
+
+
+def try_plot_activations_density():
+    """Just try it"""
+    Z_hat = np.random.randn(3, 1, 1000)
+    Z_hat[Z_hat < 2] = 0
+    Z_hat[:, :, :500] /= 6.
+
+    plot_activations_density(Z_hat, 150, plot_activations=True)
+    plt.show()
 
 
 def plot_data(X, plot_types=None):
