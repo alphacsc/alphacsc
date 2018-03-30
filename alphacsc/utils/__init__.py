fcfbd5d97bab42a4031c9cd8b727ec765d8a3096
Thomas Moreau <toma.moral@gmail.com>
2018-03-30 15:10:42 +0200

    CLN silence flake8 errors in __init__

--- alphacsc/utils/__init__.py
+++ alphacsc/utils/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa F401
 from .convolution import construct_X, construct_X_multi, _choose_convolve
 from .validation import check_random_state, check_consistent_shape
 from .viz import plot_activations_density, plot_data, plot_callback
