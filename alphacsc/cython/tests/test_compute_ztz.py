f1fda3828476a60d3b261de36ccc65c2748e5d06
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2018-04-24 15:27:32 +0200

    TST add test for _fast_compute_ztz

---
+++ alphacsc/cython/tests/test_compute_ztz.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy import sparse
+from numpy.testing import assert_allclose
+
+from alphacsc.cython import _fast_compute_ztz
+from alphacsc.utils.compute_constants import compute_ZtZ
+
+
+def test_sparse_convolve():
+    n_times = 128
+    n_times_atom = 21
+    n_atoms = 3
+    n_times_valid = n_times - n_times_atom + 1
+    density = 0.1
+    n_trials = 4
+    Z = sparse.random(n_atoms, n_trials * n_times_valid, density=density,
+                      random_state=0)
+    Z = Z.toarray().reshape(n_atoms, n_trials, n_times_valid)
+    Z_lil = [sparse.lil_matrix(zi) for zi in np.swapaxes(Z, 0, 1)]
+
+    ztz_0 = _fast_compute_ztz(Z_lil, n_times_atom)
+    ztz_1 = compute_ZtZ(Z, n_times_atom)
+    assert_allclose(ztz_0, ztz_1, atol=1e-16)
