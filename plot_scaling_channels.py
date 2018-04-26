54a652aa4ef8daf90b4a3f7ec3329a5920044d49
Thomas Moreau <toma.moral@gmail.com>
2018-04-26 18:42:22 +0200

    EXP scaling of our method with n_channel

---
+++ plot_scaling_channels.py
@@ -0,0 +1,99 @@
+import os
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+font = {'size': 14}
+matplotlib.rc('font', **font)
+
+
+def aggregate_timing(times, aggregate_method='mean'):
+    if aggregate_method == 'mean':
+        return np.mean(times)
+    elif aggregate_method == 'median':
+        return np.median(times)
+    elif aggregate_method == 'max':
+        return np.max(times)
+    else:
+        raise ValueError('unknown aggregate_time: {}'.format(aggregate_method))
+
+
+def plot_convergence(all_results_df, threshold, aggregate_method, save_name):
+    save_name += '_{}'.format(aggregate_method)
+    span_channels = all_results_df['n_channels'].unique()
+    for n_atoms in all_results_df['n_atoms'].unique():
+        for n_times_atom in all_results_df['n_times_atom'].unique():
+            for label in all_results_df['label'].unique():
+                this_res = all_results_df
+                this_res = this_res[this_res['n_atoms'] == n_atoms]
+                this_res = this_res[this_res['n_times_atom'] == n_times_atom]
+                this_res = this_res[this_res['label'] == label]
+
+                if this_res.size == 0:
+                    continue
+
+                # draw a different figure for each setting
+                fig = plt.figure(figsize=(12, 9))
+                ax = fig.gca()
+                plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+                timing_z_update = []
+                timing_d_update = []
+                timing_full_update = []
+                for n_channels in span_channels:
+                    # aggregate all runs (different random states)
+                    this_res_2 = this_res[this_res['n_channels'] == n_channels]
+                    times = this_res_2['times'].values[0]
+
+                    timing_z_update.append(aggregate_timing(times[::2], 
+                                                            aggregate_method))
+                    timing_d_update.append(aggregate_timing(times[1::2],
+                                                            aggregate_method))
+                    timing_full_update.append(np.sum(times).sum())
+
+                ax.set_yscale('log')
+                plt.plot(span_channels, timing_z_update, label="z_update")
+                plt.plot(span_channels, timing_d_update, label="d_update")
+                plt.plot(span_channels, timing_full_update, label="full_update")
+
+                plt.xlabel('n_channels')
+                plt.ylabel('Time (s)')
+                plt.legend()
+                # plt.ylim(ymin=ymin / 10)
+                # plt.title('K = %d, L = %d' % (n_atoms, n_times_atom))
+                plt.gca().tick_params(axis='x', which='both', bottom='off',
+                                      top='off')
+                plt.gca().tick_params(axis='y', which='both', left='off',
+                                      right='off')
+                plt.grid(True)
+                plt.tight_layout()
+
+                fig.savefig(save_name + '_bench_%s_K%d_L%d.png' %
+                            (label, n_atoms, n_times_atom), dpi=150)
+
+
+##############################################################################
+# load the results from file
+
+load_name = 'methods_scaling.pkl'
+
+load_name = os.path.join('figures', load_name)
+all_results_df = pd.read_pickle(load_name)
+
+if 'threshold' not in globals():
+    threshold = (all_results_df['stopping_pobj'] / all_results_df['best_pobj']
+                 - 1).unique()
+    assert threshold.size == 1
+    threshold = threshold[0]
+
+# force threshold
+threshold = 0.001
+normalize_method = None
+save_name = load_name[:-4]
+
+for aggregate_method in ['mean', 'median', 'max']:
+    plot_convergence(all_results_df, threshold, aggregate_method, save_name)
+
+# plt.show()
+plt.close('all')
---
+++ run_scaling_channels.py
@@ -0,0 +1,176 @@
+from __future__ import print_function
+import os
+import itertools
+import time
+
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from sklearn.externals.joblib import Parallel, delayed, Memory
+
+from alphacsc.utils.profile_this import profile_this  # noqa
+from alphacsc.utils import check_random_state, get_D
+from alphacsc.learn_d_z_multi import learn_d_z_multi
+
+mem = Memory(cachedir='.', verbose=0)
+
+START = time.time()
+
+##############################
+# Parameters of the simulation
+verbose = 1
+
+# n_jobs for the parallel running of single core methods
+n_jobs = 20
+# number of random states
+n_states = 1
+
+n_trials = 10  # N
+n_times_atom = 128  # L
+n_times = 20000  # T
+n_atoms = 2  # K
+reg = 1.0
+random_state = 42
+
+# A method stops if its objective function reaches best_pobj * (1 + threshold)
+threshold = .0001
+
+save_name = 'methods_scaling.pkl'
+if not os.path.exists("figures"):
+    os.mkdir("figures")
+save_name = os.path.join('figures', save_name)
+
+
+def generate_D_init(n_channels):
+    rng = check_random_state(random_state)
+    return rng.randn(n_atoms, n_channels + n_times_atom)
+
+
+def find_best_pobj(X, n_channels):
+
+    solver_z_kwargs = dict(max_iter=500, tol=1e-2)
+    D_init = generate_D_init(n_channels)
+    print('Finding best_pobj for n_channels={}...'.format(n_channels))
+    pobj, _, _, _ = learn_d_z_multi(
+        X[:, :n_channels, :], n_atoms, n_times_atom,
+        solver_z='l_bfgs', solver_z_kwargs=solver_z_kwargs,
+        solver_d='alternate_adaptive', solver_d_kwargs=dict(max_iter=50),
+        uv_constraint='separate', reg=reg, n_iter=10, random_state=0,
+        D_init=D_init, n_jobs=1, stopping_pobj=None, verbose=verbose)
+    best_pobj = pobj[-1]
+    print('[Done] n_channels={}'.format(n_channels))
+    return n_channels, best_pobj
+
+
+# @profile_this
+def run_multichannel(X, D_init, reg, n_iter, random_state,
+                     label, n_channels, stopping_pobj):
+    n_atoms, n_channels_n_times_atom = D_init.shape
+    n_times_atom = n_channels_n_times_atom - n_channels
+
+    solver_z_kwargs = dict(max_iter=500, tol=1e-1)
+    return learn_d_z_multi(
+        X, n_atoms, n_times_atom, reg=reg, n_iter=n_iter,
+        uv_constraint='separate', rank1=True, D_init=D_init,
+        solver_d='alternate_adaptive', solver_d_kwargs=dict(max_iter=50),
+        solver_z='gcd', solver_z_kwargs=solver_z_kwargs, use_sparse_z=True,
+        stopping_pobj=stopping_pobj,
+        random_state=random_state, n_jobs=1, verbose=verbose)
+
+
+# @profile_this
+def run_multivariate(X, D_init, reg, n_iter, random_state,
+                     label, n_channels, stopping_pobj):
+    n_atoms, n_channels_n_times_atom = D_init.shape
+    n_times_atom = n_channels_n_times_atom - n_channels
+    D_init = get_D(D_init, n_channels)
+
+    solver_z_kwargs = dict(max_iter=500, tol=1e-1)
+    return learn_d_z_multi(
+        X, n_atoms, n_times_atom, reg=reg, n_iter=n_iter,
+        uv_constraint='separate', rank1=False, D_init=D_init,
+        solver_d='lbfgs', solver_d_kwargs=dict(max_iter=50),
+        solver_z='gcd', solver_z_kwargs=solver_z_kwargs, use_sparse_z=True,
+        stopping_pobj=stopping_pobj,
+        random_state=random_state, n_jobs=1, verbose=verbose)
+
+
+n_iter = 50
+methods = [
+    [run_multichannel, 'rank1', n_iter],
+    # [run_multichannel_alt_gcd, 'multiCSC_gcd', n_iter // 3],
+    [run_multivariate, 'dense', n_iter],
+]
+
+
+def one_run(X, n_channels, method, n_atoms, n_times_atom,
+            stopping_pobj, best_pobj, reg=reg):
+    func, label, n_iter = method
+    current_time = time.time() - START
+    print('%s - %s: started at %.0f sec' % (n_channels, label, current_time))
+
+    # use the same init for all methods
+    D_init = generate_D_init(n_channels)
+    X = X[:, :n_channels]
+
+    # run the selected algorithm with one iter to remove compilation overhead
+    _, _, _, _ = func(X, D_init, reg, 1, random_state, label, n_channels,
+                      stopping_pobj)
+
+    # run the selected algorithm
+    pobj, times, d_hat, z_hat = func(X, D_init, reg, n_iter, random_state,
+                                     label, n_channels, stopping_pobj)
+
+    # store z_hat in a sparse matrix to reduce size
+    for z in z_hat:
+        z[z < 1e-3] = 0
+    z_hat = [sp.csr_matrix(z) for z in z_hat]
+
+    current_time = time.time() - START
+    print('%s - %s: done at %.0f sec' % (n_channels, label, current_time))
+    return (n_channels, random_state, label, np.asarray(pobj), np.asarray(times),
+            np.asarray(d_hat), np.asarray(z_hat), n_atoms, n_times_atom,
+            n_trials, n_times, stopping_pobj, best_pobj)
+
+
+if __name__ == '__main__':
+
+    cached_one_run = mem.cache(func=one_run)
+    cached_best_pobj = mem.cache(func=find_best_pobj)
+
+    all_results = []
+    # load somato data
+    from alphacsc.datasets.somato import load_data
+    X, info = load_data(epoch=False, n_jobs=n_jobs)
+
+    n_channels = X.shape[1]
+    span_channels = np.linspace(1, n_channels, 20).astype(int)
+
+    with Parallel(n_jobs=n_jobs) as parallel:
+        # Finding the best_pobj for each n_channels
+        delayed_best_pobj = delayed(cached_best_pobj)
+        best_pobjs = parallel(delayed_best_pobj(X, n_chan)
+                              for n_chan in span_channels)
+
+        stopping_pobj = [
+            (n_channels, chan_best_pobj * (1 + threshold), chan_best_pobj)
+            for n_channels, chan_best_pobj in best_pobjs]
+
+        iterator = itertools.product(methods, stopping_pobj)
+        # run the methods for different random_state
+        delayed_one_run = delayed(cached_one_run)
+        results = parallel(
+            delayed_one_run(X, n_channels, method, n_atoms,
+                            n_times_atom, stopping_pobj, best_pobj)
+            for method, (n_channels, stopping_pobj, best_pobj) in iterator)
+
+        all_results.extend(results)
+
+    # save even intermediate results
+    all_results_df = pd.DataFrame(
+        all_results, columns='n_channels random_state label pobj times '
+        'd_hat z_hat n_atoms n_times_atom n_trials n_times '
+        'stopping_pobj best_pobj'.split(' '))
+    all_results_df.to_pickle(save_name)
+
+    print('-- End of the script --')
