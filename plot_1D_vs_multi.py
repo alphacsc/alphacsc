ca143c7166541566e288932eddfba859bd782318
Thomas Moreau <toma.moral@gmail.com>
2018-05-13 18:02:07 +0200

    EXP add more random_state to smooth the n_channels curves

--- plot_1D_vs_multi.py
+++ plot_1D_vs_multi.py
@@ -16,13 +16,24 @@ if __name__ == "__main__":
     fname = args.fname
     all_results_df = pd.read_pickle(fname)
 
-    sigma = np.array(all_results_df.sigma)
-    score_d = np.array(all_results_df.score_d)
-    score_uv = np.array(all_results_df.score_uv)
-
     fig = plt.figure(figsize=(12, 9))
-    plt.loglog(sigma, np.sqrt(score_uv), label="multivariate")
-    plt.loglog(sigma, np.sqrt(score_d), label="univariate")
+    span_n_channels = all_results_df.run_n_channels.unique()
+    span_sigma = all_results_df.sigma.unique()
+    for n_channels in span_n_channels:
+        curve = []
+        results_n_channel = all_results_df[
+            all_results_df['run_n_channels'] == n_channels]
+        print("{} -> {}".format(n_channels,
+                                results_n_channel.reg.loc[results_n_channel.score.idxmin()]))
+        for sigma in span_sigma:
+            results = results_n_channel[results_n_channel['sigma'] == sigma]
+            curve += [results.score.min()]
+        plt.loglog(span_sigma, curve, label=n_channels)
+
+    # score_d = np.array(all_results_df.score_d)
+    # score_uv = np.array(all_results_df.score_uv)
+    # plt.loglog(sigma, np.sqrt(score_uv), label="multivariate")
+    # plt.loglog(sigma, np.sqrt(score_d), label="univariate")
     plt.legend()
     plt.ylabel("$\\frac{\||D - \hat D||_2}{||D||_2}$", fontsize=18)
     plt.xlabel("$\sigma$", fontsize=18)
--- run_1D_vs_multi.py
+++ run_1D_vs_multi.py
@@ -9,7 +9,7 @@ from alphacsc.simulate import get_atoms
 from alphacsc.update_d_multi import prox_uv
 from alphacsc.utils import construct_X_multi
 from alphacsc.learn_d_z_multi import learn_d_z_multi
-from alphacsc.utils.dictionary import get_D, get_lambda_max, get_uv
+from alphacsc.utils.dictionary import get_D, get_lambda_max
 
 verbose = 1
 random_state = 27
@@ -90,25 +90,52 @@ def score_d(uv, d_hat, n_channels):
     return compute_score(v, v_hat)
 
 
-def run_one(X, uv_init, reg, sigma, seed, uv, n_atoms, n_times_atom,
-            n_channels, n_iter, run_n_channels):
+def get_signals(n_channels=50, n_times_atom=64, n_times_valid=640,
+                sigma=.01, random_state=None):
 
-    rng = np.random.RandomState(seed)
+    n_atoms = 2
+    rng = np.random.RandomState(random_state)
+
+    v0 = get_atoms('triangle', n_times_atom)  # temporal atoms
+    v1 = get_atoms('square', n_times_atom)
+
+    u0 = get_atoms('sin', n_channels)  # spatial maps
+    u1 = get_atoms('cos', n_channels)
+    u0[0] = u1[0] = 1
+
+    uv = np.array([np.r_[u0, v0], np.r_[u1, v1]])
+    uv = prox_uv(uv, 'separate', n_channels)
+
+    # add atoms
+    Z = np.array([sparse.random(n_atoms, n_times_valid, density=.05,
+                                random_state=random_state).toarray()
+                 for _ in range(n_trials)])
+    Z = np.swapaxes(Z, 0, 1)
+
+    X = construct_X_multi(Z, uv, n_channels=n_channels)
+
     X = X + sigma * rng.randn(*X.shape)
 
-    if isinstance(uv_init, list):
-        D = np.zeros((n_atoms, n_channels, n_times_atom))
-        for k, i0, t0 in uv_init:
-            D[k] = X[i0, :, t0:t0 + n_times_atom]
-        uv_init = get_uv(D)
+    uv_init = rng.randn(n_atoms, n_channels + n_times_atom)
+    uv_init = prox_uv(uv_init, uv_constraint='separate', n_chan=n_channels)
+
+    return X, uv, uv_init
+
+
+def run_one(reg, sigma, n_atoms, n_times_atom, max_n_channels, n_times_valid,
+            n_iter, run_n_channels, random_state):
+
+    X, uv, uv_init = get_signals(max_n_channels, n_times_atom, n_times_valid,
+                                 sigma, random_state)
 
     reg_ = reg * get_lambda_max(X, uv_init).max()
     reg_ *= run_n_channels
 
     uv_init_ = prox_uv(np.c_[uv_init[:, :run_n_channels],
-                             uv_init[:, n_channels:]])
-    uv_ = prox_uv(np.c_[uv[:, :run_n_channels], uv[:, n_channels:]],
-                  uv_constraint='separate', n_chan=n_channels)
+                             uv_init[:, max_n_channels:]])
+    uv_ = prox_uv(np.c_[uv[:, :run_n_channels], uv[:, max_n_channels:]],
+                  uv_constraint='separate', n_chan=max_n_channels)
+
     def cb(X, uv_hat, Z_hat, pobj):
         it = len(pobj) // 2
         if it % 10 == 0:
@@ -132,7 +159,7 @@ def run_one(X, uv_init, reg, sigma, seed, uv, n_atoms, n_times_atom,
           .format(run_n_channels, reg, len(pobj) // 2, sigma, score)
           + "=" * 79)
 
-    return seed, sigma, run_n_channels, score, uv, uv_hat, reg
+    return random_state, sigma, run_n_channels, score, uv, uv_hat, reg
 
 
 if __name__ == "__main__":
@@ -155,65 +182,35 @@ if __name__ == "__main__":
     delayed_run_one = delayed(cached_run_one)
 
     # Generate synchronous D
-    n_times_atom, n_times = 64, 512
-    n_times_atom_gen = 64
-    n_times_valid = n_times - n_times_atom + 1
-    n_channels = 50
+    n_times_atom = 64
+    n_times_valid = 640
+    max_n_channels = 50
     n_atoms = 2
+    n_run = 10
     n_trials = 100
     strongest_ch = 0
 
-    n_iter = 200
+    n_iter = 500
 
-    v0 = get_atoms('triangle', n_times_atom_gen)  # temporal atoms
-    v1 = get_atoms('square', n_times_atom_gen)
+    span_reg = np.logspace(-5, -0.5, 15)
+    span_random_state = np.arange(n_run)
+    span_noise = np.logspace(-6, -1, 20)
+    span_channels = np.round(np.logspace(0, np.log10(max_n_channels), 10)
+                             ).astype(int)
 
-    u0 = get_atoms('sin', n_channels)  # spatial maps
-    u1 = get_atoms('cos', n_channels)
-    u0[strongest_ch] = u1[strongest_ch] = 1
-
-    uv = np.array([np.r_[u0, v0], np.r_[u1, v1]])
-    uv = prox_uv(uv, 'separate', n_channels)
-
-    # add atoms
-    shape_Z = (n_atoms, n_trials, n_times_atom)
-    Z = np.array([sparse.random(n_atoms, n_times_valid, density=.05,
-                                random_state=random_state).toarray()
-                 for _ in range(n_trials)])
-    Z = np.swapaxes(Z, 0, 1)
-    assert np.all(Z >= 0)
-
-    X = construct_X_multi(Z, uv, n_channels=n_channels)
-
-    reg = 0.005
-
-    rng = np.random.RandomState(1729)
-    if args.chunks:
-        uv_init = []
-        for i_atom in range(n_atoms):
-            i0 = rng.randint(n_trials)
-            t0 = rng.randint(n_times - n_times_atom)
-            uv_init += [(i_atom, i0, t0)]
-    else:
-        uv_init = rng.randn(n_atoms, n_channels + n_times_atom)
-        uv_init = prox_uv(uv_init, uv_constraint='separate', n_chan=n_channels)
-
-    span_reg = [1e-3, 2e-3, 5e-3, 8e-3, 1e-2, 3e-2]
-    span_noise = [(seed, sigma)
-                  for seed, sigma in enumerate(np.logspace(-6, -1, 20))]
-    span_channels = [1, 2, 5, 10, 25, 50]
-    grid_args = itertools.product(span_noise, span_channels, span_reg)
+    grid_args = itertools.product(span_noise, span_channels, span_reg,
+                                  span_random_state)
 
     results = Parallel(n_jobs=args.njobs)(
-        delayed_run_one(X, uv_init, reg_, sigma, seed, uv, n_atoms,
-                        n_times_atom, n_channels, n_iter, run_n_channels)
-        for (seed, sigma), run_n_channels, reg_ in grid_args
+        delayed_run_one(reg_, sigma, n_atoms, n_times_atom, max_n_channels,
+                        n_times_valid, n_iter, run_n_channels, random_state)
+        for sigma, run_n_channels, reg_, random_state in grid_args
     )
 
     # save even intermediate results
     all_results_df = pd.DataFrame(
         results,
-        columns='seed sigma run_n_channels score uv uv_hat reg'.split(' '))
+        columns='random_state sigma run_n_channels score uv uv_hat reg'.split(' '))
     all_results_df.to_pickle(save_name)
 
     # plt.plot(uv_hat[:, n_channels:].T, 'g', label='Multivariate')
