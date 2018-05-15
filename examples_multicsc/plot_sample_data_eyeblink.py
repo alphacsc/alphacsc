632800751755dabc33473ed25f0c545ca13c1164
Mainak Jas <mainakjas@gmail.com>
2018-05-15 15:39:54 +0200

    FIG for sample data. Fig 5

--- examples_multicsc/plot_sample_data_eyeblink.py
+++ examples_multicsc/plot_sample_data_eyeblink.py
@@ -12,6 +12,7 @@ from mne import EvokedArray
 from alphacsc.learn_d_z_multi import learn_d_z_multi
 from alphacsc.utils import construct_X_multi, _choose_convolve
 from alphacsc.utils import plot_callback, get_D
+from alphacsc.utils.viz import COLORS
 
 parser = argparse.ArgumentParser('Programme to launch experiment on multi csc')
 parser.add_argument('--profile', action='store_true',
@@ -21,6 +22,7 @@ args = parser.parse_args()
 dataset = 'sample'
 
 n_atoms = 25
+debug = False
 
 # get X
 data_path = op.join(mne.datasets.sample.data_path(), 'MEG', 'sample')
@@ -68,7 +70,11 @@ X *= tukey(n_times, alpha=0.1)[None, None, :]
 X /= np.linalg.norm(X, axis=-1, keepdims=True)
 
 plt.close('all')
-callback = plot_callback(X, raw.info, n_atoms)
+
+if debug:
+    callback = plot_callback(X, raw.info, n_atoms)
+else:
+    callback = None
 
 if args.profile:
     import cProfile
@@ -87,51 +93,59 @@ if args.profile:
 
 X_hat = construct_X_multi(Z_hat, uv_hat, n_channels=n_channels)
 
-plt.figure("X")
-plt.plot(X.mean(axis=1)[0])
-plt.plot(X_hat.mean(axis=1)[0])
-plt.show()
+if debug:
+    plt.figure("X")
+    plt.plot(X.mean(axis=1)[0])
+    plt.plot(X_hat.mean(axis=1)[0])
+    plt.show()
 
-# Look at v * Z for one trial
-# (we have only one trial, so full time series)
-X_hat_k = np.zeros((n_atoms, n_times))
-for k in range(n_atoms):
-    X_hat_k[k] = _choose_convolve(Z_hat[k, 0, :][None, :],
-                                  uv_hat[k, n_channels:][None, :])
-ch_names = ['atom %d' % ii for ii in range(n_atoms)]
-info = mne.create_info(ch_names, sfreq=raw.info['sfreq'])
-raw_atoms = mne.io.RawArray(X_hat_k, info, first_samp=raw.first_samp)
-raw_atoms.plot(scalings=dict(misc='auto'))
+    # Look at v * Z for one trial
+    # (we have only one trial, so full time series)
+    X_hat_k = np.zeros((n_atoms, n_times))
+    for k in range(n_atoms):
+        X_hat_k[k] = _choose_convolve(Z_hat[k, 0, :][None, :],
+                                      uv_hat[k, n_channels:][None, :])
+    ch_names = ['atom %d' % ii for ii in range(n_atoms)]
+    info = mne.create_info(ch_names, sfreq=raw.info['sfreq'])
+    raw_atoms = mne.io.RawArray(X_hat_k, info, first_samp=raw.first_samp)
+    raw_atoms.plot(scalings=dict(misc='auto'))
 
 # Plot interesting atoms
 atoms_idx = [14, 18, 23]
 times = np.arange(n_times_atom) / raw.info['sfreq']
-fig, axes = plt.subplots(1, len(atoms_idx), figsize=(12, 4))
+fig = plt.figure()
 for idx, atom_idx in enumerate(atoms_idx):
-    mne.viz.plot_topomap(uv_hat[atom_idx, :n_channels], raw.info,
-                         axes=axes[idx])
-    axes[idx].set_title('atom%d' % atom_idx)
-fig.canvas.set_window_title('Spatial atom')
 
-fig, axes = plt.subplots(len(atoms_idx), 1, sharex=True, sharey=True)
-fig.canvas.set_window_title('Temporal atom')
-for idx, atom_idx in enumerate(atoms_idx):
-    axes[idx].plot(times, uv_hat[atoms_idx[idx], n_channels:].T)
-    axes[idx].set_title('Atom%d' % atom_idx)
+    ax1 = plt.subplot2grid((len(atoms_idx), 3), (idx, 0), colspan=2)
+    ax1.plot(times, uv_hat[atoms_idx[idx], n_channels:].T, color=COLORS[0])
+    ax1.grid('on')
+
+    ax2 = plt.subplot2grid((len(atoms_idx), 3), (idx, 2), colspan=1)
+    mne.viz.plot_topomap(uv_hat[atom_idx, :n_channels], raw.info,
+                         axes=ax2)
+
+    if idx == 0:
+        ax1.set_title('A. Temporal waveform', fontsize=16)
+        ax2.set_title('B. Spatial pattern', fontsize=16)
+ax1.set_xlabel('Time (s)')
+fig.tight_layout()
+fig.savefig('figures/figure_sample.pdf')
 
 # XXX: what is this 20 Hz atom? It doesn't have topomap of motor ...
-atom_idx = 14
-plt.figure('Power spectral density')
-sfreq = raw.info['sfreq']
-psd = np.abs(np.fft.rfft(uv_hat[atom_idx, :n_channels])) ** 2
-freqs = np.linspace(0, sfreq / 2.0, psd.shape[0])
-plt.plot(freqs, psd.T)
-plt.gca().set(xscale='log')
+if debug:
+    atom_idx = 14
+    plt.figure('Power spectral density')
+    sfreq = raw.info['sfreq']
+    psd = np.abs(np.fft.rfft(uv_hat[atom_idx, :n_channels])) ** 2
+    freqs = np.linspace(0, sfreq / 2.0, psd.shape[0])
+    plt.plot(freqs, psd.T)
+    plt.gca().set(xscale='log')
 
 D = get_D(uv_hat, n_channels)[atom_idx]
 evoked = EvokedArray(D, raw.info)
 evoked.save('examples_multicsc/atom_multi_sample-ave.fif')
 
-ica.plot_properties(raw, picks=eog_inds, psd_args={'fmax': 35.},
-                    image_args={'sigma': 1.})
-ica.plot_sources(raw, exclude=eog_inds)  # look at source time course
+if debug:
+    ica.plot_properties(raw, picks=eog_inds, psd_args={'fmax': 35.},
+                        image_args={'sigma': 1.})
+    ica.plot_sources(raw, exclude=eog_inds)  # look at source time course
