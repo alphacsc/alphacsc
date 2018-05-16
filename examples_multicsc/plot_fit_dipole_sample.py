f512278a2e7c0e325fbd5291a2a9abcf0715ff98
Mainak Jas <mainakjas@gmail.com>
2018-05-16 16:39:43 +0200

    FIX faster dipole fit for sample data

--- examples_multicsc/plot_fit_dipole_sample.py
+++ examples_multicsc/plot_fit_dipole_sample.py
@@ -13,31 +13,37 @@ from alphacsc.utils.viz import COLORS
 
 data_path = mne.datasets.sample.data_path()
 subjects_dir = op.join(data_path, 'subjects')
-fname_ave = 'examples_multicsc/atom_multi_sample-ave.fif'
+fname_info = 'examples_multicsc/info_sample.fif'
 fname_cov = op.join(data_path, 'MEG', 'sample', 'sample_audvis-cov.fif')
 fname_bem = op.join(subjects_dir, 'sample', 'bem', 'sample-5120-bem-sol.fif')
 fname_trans = op.join(data_path, 'MEG', 'sample',
                       'sample_audvis_raw-trans.fif')
 fname_surf_lh = op.join(subjects_dir, 'sample', 'surf', 'lh.white')
 
-evoked = mne.read_evokeds(fname_ave, baseline=None)[0]
-evoked.pick_types(meg=True, eeg=False)
+atom_idx = 7
+
+info = mne.io.read_info(fname_info)
+data = np.load('examples_multicsc/multi_sample-ave.npz')
+n_channels, uv_hat = data['n_channels'], data['uv_hat']
+
+evoked = mne.EvokedArray(uv_hat[atom_idx, :n_channels][:, None], info)
 
 # Fit a dipole
 dip = mne.fit_dipole(evoked, fname_cov, fname_bem, fname_trans)[0]
 
 # Plot the result in 3D brain with the MRI image.
-fig = plt.figure(figsize=plt.figaspect(2.))
-ax = fig.add_subplot(2, 1, 1, projection='3d')
+fig = plt.figure(figsize=plt.figaspect(.5))
+ax = fig.add_subplot(1, 2, 1, projection='3d')
 dip.plot_locations(fname_trans, 'sample', subjects_dir, ax=ax,
                    mode='orthoview')
 best_idx = np.argmax(dip.gof)
 best_time = dip.times[best_idx]
-ax.set_title('Dipole fit (Highest GOF=%0.1f%%)' % dip.gof[best_idx])
+ax.set_title('Dipole fit')
+print('GOF = %f' % dip.gof[best_idx])
 
 # add PSD
-ax = fig.add_subplot(2, 1, 2)
-v_hat = get_uv(evoked.data[None, ...])[0, evoked.info['nchan']:]
+ax = fig.add_subplot(1, 2, 2)
+v_hat = uv_hat[atom_idx, n_channels:]
 psd = np.abs(np.fft.rfft(v_hat)) ** 2
 frequencies = np.linspace(0, evoked.info['sfreq'] / 2.0, len(psd))
 ax.plot(frequencies, 10 * np.log10(psd),
