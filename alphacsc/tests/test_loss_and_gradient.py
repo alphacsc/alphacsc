2f82975e1866ff478f014871a6ca57fd3925f619
tomMoral <thomas.moreau.2010@gmail.com>
2018-09-10 12:47:26 +0200

    CLN remove alphacsc.tests module

--- alphacsc/tests/test_loss_and_gradient.py
+++ alphacsc/tests/test_loss_and_gradient.py
@@ -1,16 +1,15 @@
 import pytest
 import numpy as np
+from functools import partial
+from scipy.optimize import approx_fprime
 
-
-from alphacsc.utils import construct_X_multi
-from alphacsc.utils.whitening import whitening
-from alphacsc.loss_and_gradient import compute_X_and_objective_multi
-from alphacsc.loss_and_gradient import gradient_d
-from alphacsc.loss_and_gradient import gradient_zi
-
-from alphacsc.tests.helper_functions import gradient_checker
 
 from alphacsc.utils import get_D
+from alphacsc.utils import construct_X_multi
+from alphacsc.utils.whitening import whitening
+from alphacsc.loss_and_gradient import gradient_d
+from alphacsc.loss_and_gradient import gradient_zi
+from alphacsc.loss_and_gradient import compute_X_and_objective_multi
 
 
 def _gradient_zi(X, z, D, loss, loss_params, flatten=False):
@@ -31,6 +30,43 @@ def _objective(X, z, D, loss, loss_params):
 def _gradient_d(X, z, D, loss, loss_params, flatten=False):
     return gradient_d(D, X, z, loss=loss, flatten=flatten,
                       loss_params=loss_params)
+
+
+def gradient_checker(func, grad, shape, args=(), kwargs={}, n_checks=10,
+                     rtol=1e-5, grad_name='gradient', debug=False,
+                     random_seed=None):
+    """Check that the gradient correctly approximate the finite difference
+    """
+
+    rng = np.random.RandomState(random_seed)
+
+    msg = ("Computed {} did not match gradient computed with finite "
+           "difference. Relative error is {{}}".format(grad_name))
+
+    func = partial(func, **kwargs)
+
+    def test_grad(z0):
+        grad_approx = approx_fprime(z0, func, 1e-8, *args)
+        grad_compute = grad(z0, *args, **kwargs)
+        error = np.sum((grad_approx - grad_compute) ** 2)
+        error /= np.sum(grad_approx ** 2)
+        error = np.sqrt(error)
+        try:
+            assert error < rtol, msg.format(error)
+        except AssertionError:
+            if debug:
+                import matplotlib.pyplot as plt
+                plt.plot(grad_approx)
+                plt.plot(grad_compute)
+                plt.show()
+            raise
+
+    z0 = np.zeros(shape)
+    test_grad(z0)
+
+    for _ in range(n_checks):
+        z0 = rng.randn(shape)
+        test_grad(z0)
 
 
 @pytest.mark.parametrize('loss', ['l2', 'dtw', 'whitening'])
