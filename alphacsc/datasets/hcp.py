2c3d04e9ec05c7194266ee4298e487834075fbe0
tomMoral <thomas.moreau.2010@gmail.com>
2018-10-11 12:43:53 +0200

    FIX db for HCP only load existing runs

--- alphacsc/datasets/hcp.py
+++ alphacsc/datasets/hcp.py
@@ -34,7 +34,7 @@ def get_all_records(hcp_path):
 
     """
     # List all files with unprocesses
-    pattern = os.path.join(hcp_path, "*/unprocessed/MEG/*")
+    pattern = os.path.join(hcp_path, "*/unprocessed/MEG/*/4D/config")
     list_files = glob(pattern)
     db = {}
     pattern = pattern.replace("*", "(.*)")
