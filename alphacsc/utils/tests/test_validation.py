f163d008414008641360c999ecce4d84d393c51f
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2019-06-29 13:40:28 +0200

    better test

--- alphacsc/utils/tests/test_validation.py
+++ alphacsc/utils/tests/test_validation.py
@@ -6,7 +6,9 @@ from alphacsc.utils import check_dimension
 
 def test_check_dimension():
     X = np.empty((2, 3, 4))
-    check_dimension(X, expected_shape="n_trials, n_channels, n_times")
-    check_dimension(X[0], expected_shape="n_trials, n_channels")
+    dims = check_dimension(X, expected_shape="n_trials, n_channels, n_times")
+    assert dims == X.shape
+    dims = check_dimension(X[0], expected_shape="n_trials, n_channels")
+    assert dims == X.shape[1:]
     with pytest.raises(ValueError):
         check_dimension(X, expected_shape="n_trials, n_channels")
