5419f1a2d34fea9293b4e1761f23e6d84e844107
Hande Gözükan <hande.gozukan@inria.fr>
2021-09-13 15:04:19 +0200

    align parameters

--- alphacsc/tests/test_encoder.py
+++ alphacsc/tests/test_encoder.py
@@ -61,64 +61,98 @@ def test_get_encoder_for_error_solver_z(X, D_hat, loss_params, solver_z):
     """Tests for invalid values of `solver_z`."""
 
     with pytest.raises(ValueError) as error:
-        get_z_encoder_for(solver=solver_z, z_kwargs=None, X=X,
-                          D_hat=D_hat, n_atoms=N_ATOMS,
-                          atom_support=N_TIMES_ATOM, algorithm='batch',
-                          reg=None, loss='l2',
-                          loss_params=loss_params, uv_constraint='joint',
+        get_z_encoder_for(solver=solver_z,
+                          z_kwargs=None,
+                          X=X,
+                          D_hat=D_hat,
+                          n_atoms=N_ATOMS,
+                          atom_support=N_TIMES_ATOM,
+                          algorithm='batch',
+                          reg=None,
+                          loss='l2',
+                          loss_params=loss_params,
+                          uv_constraint='joint',
                           feasible_evaluation=True,
-                          n_jobs=2, use_sparse_z=False)
+                          n_jobs=2,
+                          use_sparse_z=False)
         assert error.value.args[0] == f'unrecognized solver type: {solver_z}.'
 
 
 def test_get_encoder_for_error_X(X, D_hat, loss_params):
     """Tests for invalid value of `X`."""
     with pytest.raises(ValueError) as error:
-        get_z_encoder_for(solver='lgcd', z_kwargs=None, X=None,
-                          D_hat=D_hat, n_atoms=N_ATOMS,
-                          atom_support=N_TIMES_ATOM, algorithm='batch',
-                          reg=None, loss='l2',
-                          loss_params=loss_params, uv_constraint='joint',
+        get_z_encoder_for(solver='lgcd',
+                          z_kwargs=None,
+                          X=None,
+                          D_hat=D_hat,
+                          n_atoms=N_ATOMS,
+                          atom_support=N_TIMES_ATOM,
+                          algorithm='batch',
+                          reg=None,
+                          loss='l2',
+                          loss_params=loss_params,
+                          uv_constraint='joint',
                           feasible_evaluation=True,
-                          n_jobs=2, use_sparse_z=False)
+                          n_jobs=2,
+                          use_sparse_z=False)
         assert error.value.args[
             0] == 'X should be a valid array of shape (n_trials, n_channels, n_times)'
 
 
 def test_get_cost(X, D_hat, loss_params):
 
-    with get_z_encoder_for(solver='lgcd', z_kwargs=None, X=X,
-                           D_hat=D_hat, n_atoms=N_ATOMS,
-                           atom_support=N_TIMES_ATOM, algorithm='batch',
-                           reg=None, loss='l2',
-                           loss_params=loss_params, uv_constraint='joint',
+    with get_z_encoder_for(solver='lgcd',
+                           z_kwargs=None,
+                           X=X,
+                           D_hat=D_hat,
+                           n_atoms=N_ATOMS,
+                           atom_support=N_TIMES_ATOM,
+                           algorithm='batch',
+                           reg=None,
+                           loss='l2',
+                           loss_params=loss_params,
+                           uv_constraint='joint',
                            feasible_evaluation=True,
-                           n_jobs=2, use_sparse_z=False) as z_encoder:
-
+                           n_jobs=2,
+                           use_sparse_z=False) as z_encoder:
         assert not z_encoder.get_z_hat().any()
 
 
 def test_compute_z(X, D_hat, loss_params):
-    with get_z_encoder_for(solver='lgcd', z_kwargs=None, X=X,
-                           D_hat=D_hat, n_atoms=N_ATOMS,
-                           atom_support=N_TIMES_ATOM, algorithm='batch',
-                           reg=None, loss='l2',
-                           loss_params=loss_params, uv_constraint='joint',
+    with get_z_encoder_for(solver='lgcd',
+                           z_kwargs=None,
+                           X=X,
+                           D_hat=D_hat,
+                           n_atoms=N_ATOMS,
+                           atom_support=N_TIMES_ATOM,
+                           algorithm='batch',
+                           reg=None,
+                           loss='l2',
+                           loss_params=loss_params,
+                           uv_constraint='joint',
                            feasible_evaluation=True,
-                           n_jobs=2, use_sparse_z=False) as z_encoder:
+                           n_jobs=2,
+                           use_sparse_z=False) as z_encoder:
         z_encoder.compute_z()
         assert z_encoder.get_z_hat().any()
 
 
 def test_add_one_atom(X, D_hat, loss_params):
 
-    with get_z_encoder_for(solver='lgcd', z_kwargs=None, X=X,
-                           D_hat=D_hat, n_atoms=N_ATOMS,
-                           atom_support=N_TIMES_ATOM, algorithm='batch',
-                           reg=None, loss='l2',
-                           loss_params=loss_params, uv_constraint='joint',
+    with get_z_encoder_for(solver='lgcd',
+                           z_kwargs=None,
+                           X=X,
+                           D_hat=D_hat,
+                           n_atoms=N_ATOMS,
+                           atom_support=N_TIMES_ATOM,
+                           algorithm='batch',
+                           reg=None,
+                           loss='l2',
+                           loss_params=loss_params,
+                           uv_constraint='joint',
                            feasible_evaluation=True,
-                           n_jobs=2, use_sparse_z=False) as z_encoder:
+                           n_jobs=2,
+                           use_sparse_z=False) as z_encoder:
         new_atom = np.random.rand(N_CHANNELS + N_TIMES_ATOM)
         z_encoder.add_one_atom(new_atom)
         n_atoms_plus_one = z_encoder.D_hat.shape[0]
