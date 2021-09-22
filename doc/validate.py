69b81740cbadfc486e10f8e5a42bebd66f33040d
Romain PRIMET <romain.primet@inria.fr>
2021-09-22 16:24:36 +0200

    use exclusions (TODO: change to whitelist instead)

--- doc/validate.py
+++ doc/validate.py
@@ -1,14 +1,27 @@
+import re
 import sys
 
+# XXX this only works with the current numpydoc master, 1.1.0 does not
+# have get_doc_object
+from numpydoc.docscrape import get_doc_object
 from numpydoc.validate import validate
 
 import alphacsc
 
+EXCLUDE_PATTERNS = []
+
+def is_excluded(name):
+    for pat in EXCLUDE_PATTERNS:
+        if re.search(pat, name) is not None:
+            return True
+    return False
+
+def get_top_level_names():
+    return {f"alphacsc.{name}" for name in dir(alphacsc) if not is_excluded(name)}
+
 
 def validate_doc():
-    # dir(alphacsc), exclude patterns
-    to_validate = [obj for obj in ["alphacsc.learn_d_z_multi.learn_d_z_multi"] ]
-    obj_errors = [validate(name) for name in to_validate]
+    obj_errors = [validate(doc_obj) for doc_obj in get_top_level_names()]
     for obj_error in obj_errors:
         for error in obj_error['errors']:
             print(f"{obj_error['file']}:{obj_error['file_line']} - {error}")
