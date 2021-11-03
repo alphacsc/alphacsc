492dcdc7e088ece31f107dbcaedb14c3e8636eb3
Hande Gözükan <hande.gozukan@inria.fr>
2021-11-03 18:02:33 +0100

    add multichannel

--- examples/dicodile/plot_gait.py
+++ examples/dicodile/plot_gait.py
@@ -151,3 +151,115 @@ np.count_nonzero(z_hat)
 
 np.correlate(X[0][0], X_hat[0][0]) / (
     np.sqrt(np.correlate(X[0][0], X[0][0]) * np.correlate(X_hat[0][0], X_hat[0][0])))
+
+###############################################################################
+# # Multichannel signals
+
+###############################################################################
+# DiCoDiLe works just as well with multi-channel signals. The gait dataset contains 16 signals (8 for each foot), in the rest of this tutorial, we’ll use three of those.
+
+# Left foot Vertical acceleration, Y rotation and X acceleration
+channels = ['LAV', 'LRY', 'LAX']
+
+###############################################################################
+# Let’s look at a small portion of multi-channel data
+
+colors = plt.rcParams["axes.prop_cycle"]()
+mc_fig, mc_ax = plt.subplots(len(channels), sharex=True)
+
+for ax, chan in zip(mc_ax, channels):
+    ax.plot(trial['data'][chan][5000:5800],
+            label=chan, color=next(colors)["color"])
+mc_fig.legend(loc="upper center")
+
+###############################################################################
+# Let’s put the data in shape for alphacsc: (n_trials, n_channels, n_times)
+
+X_mc_subset = trial['data'][channels].to_numpy().T
+
+X_mc_subset = X_mc_subset.reshape(1, *X_mc_subset.shape)
+
+print(X_mc_subset.shape)
+
+###############################################################################
+# Initialize the dictionary (note that the call is identical to the single-channel version)
+
+D_init_mc = init_dictionary(X_mc_subset, 
+                         n_atoms=8, 
+                         n_times_atom=200,
+                         rank1=False,
+                         window=True,
+                         D_init='chunk',
+                         random_state=60)
+
+print(D_init_mc.shape)
+
+###############################################################################
+# And run DiCoDiLe (note that the call is identical to the single-channel version here as well)
+
+from alphacsc import BatchCDL
+
+cdl = BatchCDL(
+     # Shape of the dictionary
+    n_atoms, 
+    n_times_atom, 
+    rank1=False,
+    uv_constraint='auto',
+    # Number of iteration for the alternate minimization and cvg threshold
+    n_iter=3, 
+    # number of workers to be used for dicodile
+    n_jobs=4,
+    # solver for the z-step
+    solver_z='dicodile', 
+    solver_z_kwargs={'max_iter': 10000}, 
+    window=True,
+    D_init= D_init_mc,
+    random_state=60)
+
+res = cdl.fit(X_mc_subset)
+
+###############################################################################
+# # Signal reconstruction (multichannel)
+
+###############################################################################
+# Now, let’s reconstruct the original signal
+
+from alphacsc.utils import construct_X_multi
+
+z_hat_mc = res._z_hat
+
+D_hat_mc = res._D_hat
+
+X_hat_mc = construct_X_multi(z_hat_mc, D_hat_mc)
+
+###############################################################################
+# Let’s visually compare a small part of the original and reconstructed signals along with the activations.
+
+z_hat_mc.shape
+
+""
+viz_start_idx = 4000
+viz_end_idx = 5800
+viz_chan = 2
+
+max_abs = np.max(np.abs(z_hat_mc), axis=-1)
+max_abs = max_abs.reshape(z_hat_mc.shape[1], 1)
+z_hat_normalized = z_hat_mc / max_abs
+
+fig_hat_mc, ax_hat_mc = plt.subplots(2, figsize=(12, 8))
+
+# plot original and constructed
+ax_hat_mc[0].plot(X_mc_subset[0][viz_chan][viz_start_idx:viz_end_idx],
+                  label='ORIGINAL')
+ax_hat_mc[0].plot(X_hat_mc[0][viz_chan][viz_start_idx:viz_end_idx],
+                  label='RECONSTRUCTED')
+
+ax_hat_mc[0].set_xlabel('time (x10ms)')
+ax_hat_mc[0].legend()
+
+# plot activations
+for idx in range(z_hat_normalized.shape[1]):
+    ax_hat_mc[1].stem(z_hat_normalized[0][idx][viz_start_idx:viz_end_idx],
+                      linefmt=f"C{idx}-",
+                      markerfmt=f"C{idx}o")
+
