001e9456dfce3edc03a77487fefdb7354c5a22a0
Hande Gözükan <hande.gozukan@inria.fr>
2021-12-06 18:34:09 +0100

    move update_uv from update_d_multi

--- alphacsc/tests/test_update_d_multi.py
+++ alphacsc/tests/test_update_d_multi.py
@@ -4,7 +4,7 @@ from scipy import optimize, signal
 
 from alphacsc.loss_and_gradient import compute_objective
 from alphacsc.loss_and_gradient import gradient_d, gradient_uv
-from alphacsc.update_d_multi import update_uv, prox_uv, _get_d_update_constants
+from alphacsc.update_d_multi import _get_d_update_constants
 from alphacsc.update_d_multi import check_solver_and_constraints
 from alphacsc.utils.whitening import whitening
 from alphacsc.utils import construct_X_multi
@@ -205,57 +205,6 @@ def test_gradient_uv(loss):
         assert np.allclose(
             gradient_uv(uv, X=X, z=z, flatten=True),
             gradient_uv(uv, constants=constants, flatten=True)), msg
-
-
-@pytest.mark.parametrize('solver_d, uv_constraint', [
-    ('joint', 'joint'), ('alternate', 'separate')
-])
-def test_update_uv(solver_d, uv_constraint):
-    # Generate synchronous D
-    n_times_atom, n_times = 10, 100
-    n_channels = 5
-    n_atoms = 2
-    n_trials = 3
-
-    rng = np.random.RandomState()
-    z = rng.normal(size=(n_trials, n_atoms, n_times - n_times_atom + 1))
-    uv0 = rng.normal(size=(n_atoms, n_channels + n_times_atom))
-    uv1 = rng.normal(size=(n_atoms, n_channels + n_times_atom))
-
-    uv0 = prox_uv(uv0)
-    uv1 = prox_uv(uv1)
-
-    X = construct_X_multi(z, D=uv0, n_channels=n_channels)
-
-    def objective(uv):
-        X_hat = construct_X_multi(z, D=uv, n_channels=n_channels)
-        return compute_objective(X, X_hat, loss='l2')
-
-    # Ensure that the known optimal point is stable
-    uv = update_uv(X, z, uv0, max_iter=1000, verbose=0)
-    cost = objective(uv)
-
-    assert np.isclose(cost, 0), "optimal point not stable"
-    assert np.allclose(uv, uv0), "optimal point not stable"
-
-    # Ensure that the update is going down from a random initialization
-    cost0 = objective(uv1)
-    uv, pobj = update_uv(X, z, uv1, debug=True, max_iter=5000, verbose=10,
-                         solver_d=solver_d, momentum=False, eps=1e-10,
-                         uv_constraint=uv_constraint)
-    cost1 = objective(uv)
-
-    msg = "Learning is not going down"
-    try:
-        assert cost1 < cost0, msg
-        # assert np.isclose(cost1, 0, atol=1e-7)
-    except AssertionError:
-        import matplotlib.pyplot as plt
-        pobj = np.array(pobj)
-        plt.semilogy(pobj)
-        plt.title(msg)
-        plt.show()
-        raise
 
 
 def test_fast_cost():
