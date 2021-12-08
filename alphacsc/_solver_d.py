73f139a7819093fb4f4c7d35357afd7f68494c26
Hande Gözükan <hande.gozukan@inria.fr>
2021-12-08 15:55:53 +0100

    reorder functions

--- alphacsc/_solver_d.py
+++ alphacsc/_solver_d.py
@@ -133,19 +133,6 @@ class BaseDSolver:
                                random_state=self.rng,
                                window=self.window)
 
-    def _set_tukey_window(self, n_times_atom):
-        raise NotImplementedError()
-
-    def _window(self, d):
-        if self.window:
-            return d * self.tukey_window
-        return d
-
-    def _dewindow(self, d):
-        if self.window:
-            return d / self.tukey_window
-        return d
-
     def update_D(self, z_encoder):
         """Learn d's in time domain.
 
@@ -187,6 +174,19 @@ class BaseDSolver:
 
         return self._window(d0)
 
+    def _set_tukey_window(self, n_times_atom):
+        raise NotImplementedError()
+
+    def _window(self, d):
+        if self.window:
+            return d * self.tukey_window
+        return d
+
+    def _dewindow(self, d):
+        if self.window:
+            return d / self.tukey_window
+        return d
+
 
 class Rank1DSolver(BaseDSolver):
     """Base class for a rank1 solver d."""
@@ -212,6 +212,31 @@ class Rank1DSolver(BaseDSolver):
                          verbose,
                          debug)
         self.rank1 = True
+
+    def get_max_error_dict(self, z_encoder):
+        """Get the maximal reconstruction error patch from the data as a new atom
+
+        This idea is used for instance in [Yellin2017]
+
+        Parameters
+        ----------
+        z_encoder : BaseZEncoder
+            ZEncoder object to be able to compute the largest error patch.
+
+        Return
+        ------
+        uvk: array, shape (n_channels + n_times_atom,)
+            New atom for the dictionary, chosen as the chunk of data with the
+            maximal reconstruction error.
+
+        [Yellin2017] BLOOD CELL DETECTION AND COUNTING IN HOLOGRAPHIC LENS-FREE
+        IMAGING BY CONVOLUTIONAL SPARSE DICTIONARY LEARNING AND CODING.
+        """
+
+        d0 = super().get_max_error_dict(z_encoder)
+
+        return prox_uv(get_uv(d0), uv_constraint=z_encoder.uv_constraint,
+                       n_channels=z_encoder.n_channels)
 
     def _set_tukey_window(self):
 
@@ -239,31 +264,6 @@ class Rank1DSolver(BaseDSolver):
 
         return objective
 
-    def get_max_error_dict(self, z_encoder):
-        """Get the maximal reconstruction error patch from the data as a new atom
-
-        This idea is used for instance in [Yellin2017]
-
-        Parameters
-        ----------
-        z_encoder : BaseZEncoder
-            ZEncoder object to be able to compute the largest error patch.
-
-        Return
-        ------
-        uvk: array, shape (n_channels + n_times_atom,)
-            New atom for the dictionary, chosen as the chunk of data with the
-            maximal reconstruction error.
-
-        [Yellin2017] BLOOD CELL DETECTION AND COUNTING IN HOLOGRAPHIC LENS-FREE
-        IMAGING BY CONVOLUTIONAL SPARSE DICTIONARY LEARNING AND CODING.
-        """
-
-        d0 = super().get_max_error_dict(z_encoder)
-
-        return prox_uv(get_uv(d0), uv_constraint=z_encoder.uv_constraint,
-                       n_channels=z_encoder.n_channels)
-
 
 class JointDSolver(Rank1DSolver):
     """A class for 'fista' or 'joint' solver_d when rank1 is True. """
@@ -288,40 +288,6 @@ class JointDSolver(Rank1DSolver):
                          random_state,
                          verbose,
                          debug)
-
-    def _grad(self, z_encoder):
-
-        def grad(uv):
-
-            uv = self._window_uv(uv.copy(), z_encoder.n_channels)
-
-            grad = gradient_uv(uv=uv,
-                               X=z_encoder.X,
-                               z=z_encoder.get_z_hat(),
-                               constants=z_encoder.get_constants(),
-                               loss=z_encoder.loss,
-                               loss_params=z_encoder.loss_params)
-
-            grad = self._window_uv(grad, z_encoder.n_channels)
-
-            return grad
-
-        return grad
-
-    def _prox(self, z_encoder):
-
-        def prox(uv, step_size=None):
-
-            uv = self._window_uv(uv, z_encoder.n_channels)
-
-            uv = prox_uv(uv, uv_constraint=z_encoder.uv_constraint,
-                         n_channels=z_encoder.n_channels)
-
-            uv = self._dewindow_uv(uv, z_encoder.n_channels)
-
-            return uv
-
-        return prox
 
     def update_D(self, z_encoder):
         """Learn d's in time domain.
@@ -363,6 +329,40 @@ class JointDSolver(Rank1DSolver):
             return uv_hat, pobj
         return uv_hat
 
+    def _grad(self, z_encoder):
+
+        def grad(uv):
+
+            uv = self._window_uv(uv.copy(), z_encoder.n_channels)
+
+            grad = gradient_uv(uv=uv,
+                               X=z_encoder.X,
+                               z=z_encoder.get_z_hat(),
+                               constants=z_encoder.get_constants(),
+                               loss=z_encoder.loss,
+                               loss_params=z_encoder.loss_params)
+
+            grad = self._window_uv(grad, z_encoder.n_channels)
+
+            return grad
+
+        return grad
+
+    def _prox(self, z_encoder):
+
+        def prox(uv, step_size=None):
+
+            uv = self._window_uv(uv, z_encoder.n_channels)
+
+            uv = prox_uv(uv, uv_constraint=z_encoder.uv_constraint,
+                         n_channels=z_encoder.n_channels)
+
+            uv = self._dewindow_uv(uv, z_encoder.n_channels)
+
+            return uv
+
+        return prox
+
 
 class AlternateDSolver(Rank1DSolver):
     """A class for 'alternate' or 'alternate_adaptive' solver_d when rank1 is
@@ -390,152 +390,6 @@ class AlternateDSolver(Rank1DSolver):
                          verbose,
                          debug)
         self.adaptive_step_size = (self.solver_d == 'alternate_adaptive')
-
-    def compute_lipschitz(self, uv0, n_channels, ztz, variable):
-
-        u0, v0 = uv0[:, :n_channels], uv0[:, n_channels:]
-        n_atoms = uv0.shape[0]
-        n_times_atom = uv0.shape[1] - n_channels
-        if not hasattr(self, 'b_hat_0'):
-            self.b_hat_0 = np.random.randn(uv0.size)
-
-        def op_Hu(u):
-            u = np.reshape(u, (n_atoms, n_channels))
-            uv = np.c_[u, v0]
-            H_d = numpy_convolve_uv(ztz, uv)
-            H_u = (H_d * uv[:, None, n_channels:]).sum(axis=2)
-            return H_u.ravel()
-
-        def op_Hv(v):
-            v = np.reshape(v, (n_atoms, n_times_atom))
-            uv = np.c_[u0, v]
-            H_d = numpy_convolve_uv(ztz, uv)
-            H_v = (H_d * uv[:, :n_channels, None]).sum(axis=1)
-            return H_v.ravel()
-
-        if variable == 'u':
-            b_hat_u0 = self.b_hat_0.reshape(
-                n_atoms, -1)[:, :n_channels].ravel()
-            n_points = n_atoms * n_channels
-            L = power_iteration(op_Hu, n_points, b_hat_0=b_hat_u0)
-        elif variable == 'v':
-            b_hat_v0 = self.b_hat_0.reshape(
-                n_atoms, -1)[:, n_channels:].ravel()
-            n_points = n_atoms * n_times_atom
-            L = power_iteration(op_Hv, n_points, b_hat_0=b_hat_v0)
-        else:
-            raise ValueError("variable should be either 'u' or 'v'")
-        return L
-
-    def _grad_u(self, v_hat, z_encoder):
-
-        def grad_u(u):
-
-            uv = np.c_[u, self._window(v_hat)]
-
-            grad_d = gradient_d(uv,
-                                X=z_encoder.X,
-                                z=z_encoder.get_z_hat(),
-                                constants=z_encoder.get_constants(),
-                                loss=z_encoder.loss,
-                                loss_params=z_encoder.loss_params)
-
-            return (grad_d * uv[:, None, z_encoder.n_channels:]).sum(axis=2)
-
-        return grad_u
-
-    def _grad_v(self, u_hat, z_encoder):
-
-        def grad_v(v):
-
-            v = self._window(v)
-
-            uv = np.c_[u_hat, v]
-
-            grad_d = gradient_d(uv,
-                                X=z_encoder.X,
-                                z=z_encoder.get_z_hat(),
-                                constants=z_encoder.get_constants(),
-                                loss=z_encoder.loss,
-                                loss_params=z_encoder.loss_params)
-
-            grad_v = (grad_d * uv[:, :z_encoder.n_channels, None]).sum(axis=1)
-
-            return self._window(grad_v)
-
-        return grad_v
-
-    def _get_step_size(self, uv_hat, loss, n_channels, ztz, variable):
-
-        if self.adaptive_step_size:
-            return None
-        else:
-            if loss != 'l2':
-                raise NotImplementedError()
-            step_size = self.compute_lipschitz(uv_hat, n_channels, ztz,
-                                               variable)
-
-            assert step_size > 0
-            return 0.99 / step_size
-
-    def _run_fista(self, d_hat, uv_hat, obj, grad, prox, variable, z_encoder):
-
-        L = self._get_step_size(uv_hat, z_encoder.loss,
-                                z_encoder.n_channels,
-                                z_encoder.ztz, variable)
-
-        return fista(obj,
-                     grad,
-                     prox,
-                     L,
-                     d_hat,
-                     self.max_iter,
-                     momentum=self.momentum,
-                     eps=self.eps,
-                     adaptive_step_size=self.adaptive_step_size,
-                     debug=self.debug,
-                     verbose=self.verbose,
-                     name="Update " + variable)
-
-    def _update_u(self, uv_hat, u_hat, v_hat, objective, z_encoder):
-
-        def prox_u(u, step_size=None):
-            u /= np.maximum(1., np.linalg.norm(u, axis=1, keepdims=True))
-            return u
-
-        def obj(u):
-            uv = np.c_[u, v_hat]
-            return objective(uv)
-
-        u_hat, pobj_u = self._run_fista(u_hat, uv_hat, obj,
-                                        self._grad_u(v_hat, z_encoder),
-                                        prox_u, 'u', z_encoder)
-
-        uv_hat = np.c_[u_hat, v_hat]
-
-        return u_hat, uv_hat, pobj_u
-
-    def _update_v(self, uv_hat, u_hat, v_hat, objective, z_encoder):
-
-        def prox_v(v, step_size=None):
-
-            v = self._window(v)
-
-            v /= np.maximum(1., np.linalg.norm(v, axis=1, keepdims=True))
-
-            return self._dewindow(v)
-
-        def obj(v):
-            uv = np.c_[u_hat, v]
-            return objective(uv)
-
-        v_hat, pobj_v = self._run_fista(v_hat, uv_hat, obj,
-                                        self._grad_v(u_hat, z_encoder),
-                                        prox_v, 'v', z_encoder)
-
-        uv_hat = np.c_[u_hat, v_hat]
-
-        return v_hat, uv_hat, pobj_v
 
     def update_D(self, z_encoder):
         """Learn d's in time domain.
@@ -583,6 +437,145 @@ class AlternateDSolver(Rank1DSolver):
             return uv_hat, pobj
         return uv_hat
 
+    def _update_u(self, uv_hat, u_hat, v_hat, objective, z_encoder):
+
+        def prox_u(u, step_size=None):
+            u /= np.maximum(1., np.linalg.norm(u, axis=1, keepdims=True))
+            return u
+
+        def obj(u):
+            uv = np.c_[u, v_hat]
+            return objective(uv)
+
+        u_hat, pobj_u = self._run_fista(u_hat, uv_hat, obj,
+                                        self._grad_u(v_hat, z_encoder),
+                                        prox_u, 'u', z_encoder)
+
+        uv_hat = np.c_[u_hat, v_hat]
+
+        return u_hat, uv_hat, pobj_u
+
+    def _grad_u(self, v_hat, z_encoder):
+
+        def grad_u(u):
+
+            uv = np.c_[u, self._window(v_hat)]
+
+            grad_d = gradient_d(uv,
+                                X=z_encoder.X,
+                                z=z_encoder.get_z_hat(),
+                                constants=z_encoder.get_constants(),
+                                loss=z_encoder.loss,
+                                loss_params=z_encoder.loss_params)
+
+            return (grad_d * uv[:, None, z_encoder.n_channels:]).sum(axis=2)
+
+        return grad_u
+
+    def _update_v(self, uv_hat, u_hat, v_hat, objective, z_encoder):
+
+        def prox_v(v, step_size=None):
+
+            v = self._window(v)
+
+            v /= np.maximum(1., np.linalg.norm(v, axis=1, keepdims=True))
+
+            return self._dewindow(v)
+
+        def obj(v):
+            uv = np.c_[u_hat, v]
+            return objective(uv)
+
+        v_hat, pobj_v = self._run_fista(v_hat, uv_hat, obj,
+                                        self._grad_v(u_hat, z_encoder),
+                                        prox_v, 'v', z_encoder)
+
+        uv_hat = np.c_[u_hat, v_hat]
+
+        return v_hat, uv_hat, pobj_v
+
+    def _grad_v(self, u_hat, z_encoder):
+
+        def grad_v(v):
+
+            v = self._window(v)
+
+            uv = np.c_[u_hat, v]
+
+            grad_d = gradient_d(uv,
+                                X=z_encoder.X,
+                                z=z_encoder.get_z_hat(),
+                                constants=z_encoder.get_constants(),
+                                loss=z_encoder.loss,
+                                loss_params=z_encoder.loss_params)
+
+            grad_v = (grad_d * uv[:, :z_encoder.n_channels, None]).sum(axis=1)
+
+            return self._window(grad_v)
+
+        return grad_v
+
+    def _run_fista(self, d_hat, uv_hat, obj, grad, prox, variable, z_encoder):
+
+        if self.adaptive_step_size:
+            L = 1
+        else:
+            if z_encoder.loss != 'l2':
+                raise NotImplementedError()
+            L = self.compute_lipschitz(uv_hat, z_encoder.n_channels,
+                                       z_encoder.ztz, variable)
+
+        assert L > 0
+
+        return fista(obj,
+                     grad,
+                     prox,
+                     0.99 / L,
+                     d_hat,
+                     self.max_iter,
+                     momentum=self.momentum,
+                     eps=self.eps,
+                     adaptive_step_size=self.adaptive_step_size,
+                     debug=self.debug,
+                     verbose=self.verbose,
+                     name="Update " + variable)
+
+    def compute_lipschitz(self, uv0, n_channels, ztz, variable):
+
+        u0, v0 = uv0[:, :n_channels], uv0[:, n_channels:]
+        n_atoms = uv0.shape[0]
+        n_times_atom = uv0.shape[1] - n_channels
+        if not hasattr(self, 'b_hat_0'):
+            self.b_hat_0 = np.random.randn(uv0.size)
+
+        def op_Hu(u):
+            u = np.reshape(u, (n_atoms, n_channels))
+            uv = np.c_[u, v0]
+            H_d = numpy_convolve_uv(ztz, uv)
+            H_u = (H_d * uv[:, None, n_channels:]).sum(axis=2)
+            return H_u.ravel()
+
+        def op_Hv(v):
+            v = np.reshape(v, (n_atoms, n_times_atom))
+            uv = np.c_[u0, v]
+            H_d = numpy_convolve_uv(ztz, uv)
+            H_v = (H_d * uv[:, :n_channels, None]).sum(axis=1)
+            return H_v.ravel()
+
+        if variable == 'u':
+            b_hat_u0 = self.b_hat_0.reshape(
+                n_atoms, -1)[:, :n_channels].ravel()
+            n_points = n_atoms * n_channels
+            L = power_iteration(op_Hu, n_points, b_hat_0=b_hat_u0)
+        elif variable == 'v':
+            b_hat_v0 = self.b_hat_0.reshape(
+                n_atoms, -1)[:, n_channels:].ravel()
+            n_points = n_atoms * n_times_atom
+            L = power_iteration(op_Hv, n_points, b_hat_0=b_hat_v0)
+        else:
+            raise ValueError("variable should be either 'u' or 'v'")
+        return L
+
 
 class DSolver(BaseDSolver):
     """A class for 'fista' solver_d when rank1 is False. """
@@ -609,74 +602,6 @@ class DSolver(BaseDSolver):
                          debug)
 
         self.rank1 = False
-
-    def _set_tukey_window(self):
-
-        if self.window:
-            self.tukey_window = tukey_window(self.n_times_atom)[None, None, :]
-
-    def _objective(self, z_encoder):
-
-        def objective(D, full=False):
-
-            D = self._window(D)
-
-            return z_encoder.compute_objective(D)
-
-        return objective
-
-    def get_max_error_dict(self, z_encoder):
-        """Get the maximal reconstruction error patch from the data as a new atom
-
-        This idea is used for instance in [Yellin2017]
-
-        Parameters
-        ----------
-        z_encoder : BaseZEncoder
-            ZEncoder object to be able to compute the largest error patch.
-
-        Return
-        ------
-        dk: array, shape (n_channels, n_times_atom,)
-            New atom for the dictionary, chosen as the chunk of data with the
-            maximal reconstruction error.
-
-        [Yellin2017] BLOOD CELL DETECTION AND COUNTING IN HOLOGRAPHIC LENS-FREE
-        IMAGING BY CONVOLUTIONAL SPARSE DICTIONARY LEARNING AND CODING.
-        """
-
-        d0 = super().get_max_error_dict(z_encoder)
-
-        return prox_d(d0)
-
-    def _grad(self, z_encoder):
-
-        def grad(D):
-
-            D = self._window(D)
-
-            grad = gradient_d(D=D,
-                              X=z_encoder.X,
-                              z=z_encoder.get_z_hat(),
-                              constants=z_encoder.get_constants(),
-                              loss=z_encoder.loss,
-                              loss_params=z_encoder.loss_params)
-
-            return self._window(grad)
-
-        return grad
-
-    def _prox(self, z_encoder):
-
-        def prox(D, step_size=None):
-
-            D = self._window(D)
-
-            D = prox_d(D)
-
-            return self._dewindow(D)
-
-        return prox
 
     def update_D(self, z_encoder):
         """Learn d's in time domain.
@@ -712,3 +637,71 @@ class DSolver(BaseDSolver):
         if self.debug:
             return D_hat, pobj
         return D_hat
+
+    def _grad(self, z_encoder):
+
+        def grad(D):
+
+            D = self._window(D)
+
+            grad = gradient_d(D=D,
+                              X=z_encoder.X,
+                              z=z_encoder.get_z_hat(),
+                              constants=z_encoder.get_constants(),
+                              loss=z_encoder.loss,
+                              loss_params=z_encoder.loss_params)
+
+            return self._window(grad)
+
+        return grad
+
+    def _prox(self, z_encoder):
+
+        def prox(D, step_size=None):
+
+            D = self._window(D)
+
+            D = prox_d(D)
+
+            return self._dewindow(D)
+
+        return prox
+
+    def get_max_error_dict(self, z_encoder):
+        """Get the maximal reconstruction error patch from the data as a new atom
+
+        This idea is used for instance in [Yellin2017]
+
+        Parameters
+        ----------
+        z_encoder : BaseZEncoder
+            ZEncoder object to be able to compute the largest error patch.
+
+        Return
+        ------
+        dk: array, shape (n_channels, n_times_atom,)
+            New atom for the dictionary, chosen as the chunk of data with the
+            maximal reconstruction error.
+
+        [Yellin2017] BLOOD CELL DETECTION AND COUNTING IN HOLOGRAPHIC LENS-FREE
+        IMAGING BY CONVOLUTIONAL SPARSE DICTIONARY LEARNING AND CODING.
+        """
+
+        d0 = super().get_max_error_dict(z_encoder)
+
+        return prox_d(d0)
+
+    def _set_tukey_window(self):
+
+        if self.window:
+            self.tukey_window = tukey_window(self.n_times_atom)[None, None, :]
+
+    def _objective(self, z_encoder):
+
+        def objective(D, full=False):
+
+            D = self._window(D)
+
+            return z_encoder.compute_objective(D)
+
+        return objective
