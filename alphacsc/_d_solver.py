f689571b394028fb50b6b1d1a27c330b355135cb
Hande Gözükan <hande.gozukan@inria.fr>
2022-02-18 10:08:44 +0100

    update doc

--- alphacsc/_d_solver.py
+++ alphacsc/_d_solver.py
@@ -271,10 +271,13 @@ class BaseDSolver:
 
         Returns
         -------
-        D_hat : array, shape (n_atoms, n_channels + n_times_atom) or
-                             (n_atoms, n_channels, n_times_atom)
-            The atoms to learn from the data.
+        D_hat : array, shape (k+1, n_channels + n_times_atom) or
+                             (k+1, n_channels, n_times_atom)
+            The atoms to learn from the data, where k < n_atoms is the initial
+        number of atoms in the dictionary before adding an atom.
         """
+        # XXX should it check the number of atoms in D_hat is smaller
+        # than n_atoms
         new_atom = self.get_max_error_dict(z_encoder)[0]
 
         self.D_hat = np.concatenate([self.D_hat, new_atom[None]])
