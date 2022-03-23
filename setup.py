922501cfe7b6645654bb822d1839ed901f7036b6
tomMoral <thomas.moreau.2010@gmail.com>
2022-03-23 16:22:19 +0100

    CLN remove unused init (SSA./KMeans)

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 #! /usr/bin/env python
-from setuptools import setup, Extension
-import numpy as np
-
-
-# Add cython extensions
-kmc2 = Extension('alphacsc.other.kmc2.kmc2',
-                 sources=['alphacsc/other/kmc2/kmc2.pyx'],
-                 extra_compile_args=['-O3'],
-                 include_dirs=[np.get_include()])
-modules = [kmc2]
+from setuptools import setup
 
 
 if __name__ == "__main__":
-    from Cython.Build import cythonize
 
-    setup(ext_modules=cythonize(modules))
+    setup()
