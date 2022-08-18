747623160308cca14a344b0ecf91242b7ad8ad17
Hande Gözükan <hande.gozukan@inria.fr>
2022-08-18 15:25:44 +0200

    change order of methods

--- benchmarks/bench_methods_run.py
+++ benchmarks/bench_methods_run.py
@@ -189,8 +189,8 @@ n_iter_multi = 20
 methods_multivariate = [
     [run_multichannel_gcd_fullrank, 'gcd fullrank', n_iter_multi],
     [run_multichannel_dicodile_fullrank, 'dicodile fullrank', n_iter_multi],
-    [run_multichannel_dicodile, 'dicodile', n_iter_multi],
     [run_multichannel_gcd, 'gcd', n_iter_multi],
+    [run_multichannel_dicodile, 'dicodile', n_iter_multi],
 ]
 
 
